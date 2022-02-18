f8b717a5c3e7926eaac0a39cc189cf38c3f0302b
Jenny Taylor <jennytaylor82@gmail.com>
2022-02-18 16:31:35 +0000

    progress bar added

--- src/studentcomponents/navbar/index.js
+++ src/studentcomponents/navbar/index.js
@@ -8,24 +8,25 @@ function Navbar({isNewMessage}) {
 
   return (
     <div className={styles.navbar}>
-    <div className={styles.student}>
-    <img src="https://cdn-icons-png.flaticon.com/512/2490/2490295.png"></img>
-    <h1>Alice</h1>
-    </div>
-        
-        <button>Home</button>
-        <button>Dictionary</button>
-        <button>Finished Books</button>
-        
-    
+      <div className={styles.student}>
+        <img src="https://cdn-icons-png.flaticon.com/512/2490/2490295.png"></img>
+        <h1>Alice</h1>
+      </div>
 
-        {isNewMessage && <img src="https://cdn-icons.flaticon.com/png/512/2524/premium/2524018.png?token=exp=1645193851~hmac=899f93c1516388a9dc9f835be892cc6e"></img>}
-        {!isNewMessage && <img src="https://cdn-icons.flaticon.com/png/512/1744/premium/1744763.png?token=exp=1645193807~hmac=b55c05da52d7f03685617109179078c2"></img>}
-        
-    
-        <button className={styles.logoutbutton}>Log Out</button>
+      <button>Home</button>
+      <button>Dictionary</button>
+      <button>Finished Books</button>
+
+      {isNewMessage && (
+        <img src="https://cdn-icons-png.flaticon.com/512/6875/6875112.png"></img>
+      )}
+      {!isNewMessage && (
+        <img src="https://cdn-icons-png.flaticon.com/512/6875/6875170.png"></img>
+      )}
+
+      <button className={styles.logoutbutton}>Log Out</button>
     </div>
-  )
+  );
 }
 
 export default Navbar
