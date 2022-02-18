4d32cc760d871f394d85957a820215cf3c158f8c
Anita Caira Percival <anita.percival1@gmail.com>
2022-02-18 14:55:45 +0000

    Navbar component done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { useState } from "react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [isNewMessage, setIsNewMessage] = useState(true);
+
+  return <Component {...pageProps} isNewMessage={isNewMessage} />
+
 }
 
 export default MyApp
