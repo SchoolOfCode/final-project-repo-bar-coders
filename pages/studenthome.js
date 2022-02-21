0356e383c333ab642c6cfd334412b13aff09ff9c
Jenny Taylor <jennytaylor82@gmail.com>
2022-02-21 13:08:24 +0000

    book carousel added including functions

--- pages/studenthome.js
+++ pages/studenthome.js
@@ -1,12 +1,27 @@
 import Navbar from "../src/studentcomponents/navbar"
 import ProgressBar from "../src/studentcomponents/progressbar"
+import Carousel from "../src/studentcomponents/bookcarousel";
 
 
-export default function StudentHome({ isNewMessage, studentDaysRead }) {
+export default function StudentHome({
+  isNewMessage,
+  studentDaysRead,
+  inProgressBooks,
+  currentBook,
+  updateCurrentBook,
+}) {
   return (
     <div>
-      <Navbar isNewMessage={isNewMessage}/>
+      <Navbar isNewMessage={isNewMessage} />
       <ProgressBar studentDaysRead={studentDaysRead} />
+
+      <div>
+        <Carousel
+          inProgressBooks={inProgressBooks}
+          currentBook={currentBook}
+          updateCurrentBook={updateCurrentBook}
+        />
+      </div>
     </div>
   );
 }
