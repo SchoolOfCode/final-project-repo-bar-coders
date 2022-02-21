e039c7e38a7908c21a7ef8127d40b186040b5a66
NovaGlow <dilyncorbett@gmail.com>
2022-02-21 12:34:00 +0000

    front end info window creation

--- pages/studenthome.js
+++ pages/studenthome.js
@@ -1,12 +1,15 @@
-import Navbar from "../src/studentcomponents/navbar"
-import ProgressBar from "../src/studentcomponents/progressbar"
-
+import Navbar from "../src/studentcomponents/navbar";
+import ProgressBar from "../src/studentcomponents/progressbar";
+import InfoWindow from "../src/studentcomponents/infowindow";
+ 
 
 export default function StudentHome({ isNewMessage, studentDaysRead }) {
   return (
     <div>
-      <Navbar isNewMessage={isNewMessage}/>
+      <Navbar isNewMessage={isNewMessage} />
       <ProgressBar studentDaysRead={studentDaysRead} />
+      <InfoWindow/>
+      
     </div>
   );
 }
