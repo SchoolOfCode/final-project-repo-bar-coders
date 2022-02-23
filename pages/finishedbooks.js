81430457b5950c363500741eb86aa9bf9b2a50a7
NovaGlow <dilyncorbett@gmail.com>
2022-02-23 10:19:58 +0000

    added new covers

--- pages/finishedbooks.js
+++ pages/finishedbooks.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import Navbar from '../src/studentcomponents/navbar'
 import Styles from '../styles/finishedbooks.module.css'
-import hp from '../images/HP.jpg' 
-import Image from 'next/image';
+
 
 function finishedbooks({ isNewMessage }) {
   return (
     <div>
         {<Navbar isNewMessage={isNewMessage} />}
           <div className={Styles.container}>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" />
+            <div className={Styles.box}><img src='BB.JPG' alt="" width="350rem" height="350rem" />
             </div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='CA.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='CW.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='GB.JPG' alt="" width="350rem" height="350rem" /></div>
            </div>
           <div className={Styles.container}>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><Image src={hp.src} alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='GE.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='HP.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='NA.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='SS.JPG' alt="" width="350rem" height="350rem" /></div>
           </div>
     </div>
   );
