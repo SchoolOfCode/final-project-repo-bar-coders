7d6b328607cf937352e7a190d873eb2a3a14274e
NovaGlow <dilyncorbett@gmail.com>
2022-02-23 11:45:41 +0000

    added dates to finished books

--- pages/finishedbooks.js
+++ pages/finishedbooks.js
@@ -8,17 +8,16 @@ function finishedbooks({ isNewMessage }) {
     <div>
         {<Navbar isNewMessage={isNewMessage} />}
           <div className={Styles.container}>
-            <div className={Styles.box}><img src='BB.JPG' alt="" width="350rem" height="350rem" />
-            </div>
-            <div className={Styles.box}><img src='CA.JPG' alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><img src='CW.JPG' alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><img src='GB.JPG' alt="" width="350rem" height="350rem" /></div>
-           </div>
+              <div className={Styles.box}><img src='BB.JPG' alt="" width="350rem" height="350rem" />Completed: 02-02-2022</div>
+              <div className={Styles.box}><img src='CA.JPG' alt="" width="350rem" height="350rem" />Completed: 15-12-2021</div>
+              <div className={Styles.box}><img src='CW.JPG' alt="" width="350rem" height="350rem" />Completed: 10-12-2021</div>
+              <div className={Styles.box}><img src='GB.JPG' alt="" width="350rem" height="350rem" />Completed: 01-01-2022</div>
+          </div>
           <div className={Styles.container}>
-            <div className={Styles.box}><img src='GE.JPG' alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><img src='HP.JPG' alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><img src='NA.JPG' alt="" width="350rem" height="350rem" /></div>
-            <div className={Styles.box}><img src='SS.JPG' alt="" width="350rem" height="350rem" /></div>
+            <div className={Styles.box}><img src='GE.JPG' alt="" width="350rem" height="350rem" />Completed: 26-05-2021</div>
+            <div className={Styles.box}><img src='HP.JPG' alt="" width="350rem" height="350rem" />Completed: 13-08-2020</div>
+            <div className={Styles.box}><img src='NA.JPG' alt="" width="350rem" height="350rem" />Completed: 12-06-2021</div>
+            <div className={Styles.box}><img src='SS.JPG' alt="" width="350rem" height="350rem" />Completed: 17-07-2021</div>
           </div>
     </div>
   );
