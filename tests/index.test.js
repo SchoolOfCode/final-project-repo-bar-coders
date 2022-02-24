72fc87678394e5c8bb66b4c69ddeef5f349ed96f
Anita Caira Percival <anita.percival1@gmail.com>
2022-02-24 10:02:17 +0000

    Testing started

---
+++ tests/index.test.js
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Index from '../pages/index'
+
+
+// Testing if login page renders
+// describe("Index page", () => {
+//   it("should render", () => {
+//       render(<Index />);
+//       // const h1 = screen.getByRole("h1");
+//       // expect(h1).toBeInTheDocument();
+//   });
+// });
+
+
+
