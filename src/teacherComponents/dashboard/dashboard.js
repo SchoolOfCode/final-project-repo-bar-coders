9ff6174447835b5b4e3e8acf9c3ad0181a5d53f1
Anita Caira Percival <anita.percival1@gmail.com>
2022-03-01 10:58:52 +0000

    Chakra

--- src/teacherComponents/dashboard/dashboard.js
+++ src/teacherComponents/dashboard/dashboard.js
@@ -4,19 +4,7 @@ import Styles from '../../../styles/teacherdashboard.module.css'
 export default function Dashboard() {
   return (
     <div className={Styles.dashboard}>
-    dashboard
-    <div className={Styles.box1}>
-      box1
-    </div>
-    <div className={Styles.box2}>
-      box2
-    </div>
-    <div className={Styles.box3}>
-      box3
-    </div>
-    <div className={Styles.box4}>
-      box4
-    </div>
+
     </div>
   )
 }
