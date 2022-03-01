9964b3b8cb54a5a5fa31122e6c8bb91ac598fd33
NovaGlow <dilyncorbett@gmail.com>
2022-03-01 12:29:52 +0000

    css up and running

--- src/teacherComponents/dashboard/dashboard.js
+++ src/teacherComponents/dashboard/dashboard.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Styles from '../../../styles/teacherdashboard.module.css'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 export default function Dashboard() {
   return (
-    <div className={Styles.dashboard}>
-
+    <div className={Styles.mainbox}>
+<Grid className={Styles.grid}
+  h='765px'
+  w='1200px'
+  templateRows='repeat(3, 2fr)'
+  templateColumns='repeat(6, 1fr)'
+  gap={5}
+  bg='silver'
+>
+    
+      <GridItem w='1130px' colSpan={4} bg='white' />
+      <GridItem w='539px'  colSpan={3} bg='white' />
+      <GridItem w='539px'  colSpan={3} bg='white' />
+      <GridItem w='539px'  colSpan={3} bg='white' />
+      <GridItem w='539px'  colSpan={3} bg='white' />
+   
+  
+</Grid>
     </div>
   )
 }
--- src/teacherComponents/navbar/navbar.js
+++ src/teacherComponents/navbar/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import Styles from '../../../styles/teachernavbar.module.css'
+import { Button, ButtonGroup, Stack, Heading, Flex} from '@chakra-ui/react'
 
 
 
@@ -8,17 +9,28 @@ import Styles from '../../../styles/teachernavbar.module.css'
 export default function Navbar() {  // add teachers name 
   return (
       <div className={Styles.navbar}>
-        <div className={Styles.name}>  
-            Welcome Back Mrs Freeman
-        </div>
+        <flex>
+         <Heading>  
+            Welcome Back,
+            </Heading>
+            <Heading> 
+            Mrs Freeman
+         </Heading>
+        </flex>
+
+        <Stack direction='row' spacing={4} align='center' paddingTop='1rem' paddingRight='1rem'>
+                <Link href="/teacherhome" passHref>
+                    <Button colorScheme='gray' variant='outline' size='lg' color='white'>
+                     HOME
+                    </Button>
+                </Link>
         
-            <Link href="/teacherhome" passHref>
-                <button>Home</button>
-            </Link>
-     
-            <Link href="/" passHref>
-                <button>Log Out</button>
-            </Link>
+                <Link href="/" passHref>
+                    <Button colorScheme='gray' variant='outline' size='lg' color='white'>
+                     LOGOUT
+                    </Button>
+                </Link>
+            </Stack>
         </div>
   )
 }
