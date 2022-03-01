a2060b39ac940ea50944c812b3b5c775715b0357
Anita Caira Percival <anita.percival1@gmail.com>
2022-03-01 16:42:18 +0000

    More info page css started

---
+++ src/teacherComponents/Teachernavbar/teachernavbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Styles from '../../../styles/teachernavbar.module.css'
+
+
+
+function Teachernavbar() {
+  return (
+    <div className={Styles.teachernavbar}>
+        <div className={Styles.leftside}>
+        <h1>Welcome, name</h1>
+        </div>
+        <div className={Styles.rightside}>
+        <button>Home</button>
+        <button>Log out</button>
+        </div>
+    </div>
+  )
+}
+
+export default Teachernavbar
---
+++ src/teacherComponents/classlist/classlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from '../../../styles/classlist.module.css'
+
+function Classlist() {
+  return (
+    <div className={Styles.classlist}>
+        <h1>Class List</h1>
+    </div>
+  )
+}
+
+export default Classlist
