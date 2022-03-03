b65d998b6a4c5d2dd0d8135ce428b0e1aaea9e19
NovaGlow <dilyncorbett@gmail.com>
2022-03-03 10:08:19 +0000

    removed teacher files

--- pages/teacherhome.js
+++ pages/teacherhome.js
@@ -5,11 +5,8 @@ import Styles from '../styles/globalteachview.module.css'
 export default function Teacherhome() {
   return (
         <div className={Styles.container}> 
-            <Classroomlist></Classroomlist>
-            <div className={Styles.rightside}>
-            <Navbar></Navbar>
-            <Dashboard></Dashboard>
-            </div>
+           
         </div>
+        
   )
 }
