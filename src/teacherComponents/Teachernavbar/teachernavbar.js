928fec5d861719c9454383440e8624bf8d56c89e
Jenny Taylor <jennytaylor82@gmail.com>
2022-03-06 16:03:10 +0000

    tweaked teacher side css to make it fit a smaller monitor

--- src/teacherComponents/Teachernavbar/teachernavbar.js
+++ src/teacherComponents/Teachernavbar/teachernavbar.js
@@ -5,25 +5,24 @@ import Link from "next/link";
 function Teachernavbar() {
   return (
     <div className={Styles.teachernavbar}>
-        <div className={Styles.leftside}>
-          <h1> Welcome, Mrs Freeman </h1>
-        </div>
-        
-        <div className={Styles.rightside}>
-          <Link href="/teacherhome" passHref>
-            <button className={Styles.home}> Class DashBoard </button>
-          </Link>
-        </div>
-    
-        <div className={Styles.rightside}>
-          <Link href="/" passHref>
-            <button className={Styles.logout}> Log out </button>
-          </Link>
-        </div> 
-    
+      <div className={Styles.leftside}>
+        <h1> Welcome, Mrs Freeman </h1>
+      </div>
+
+      <div className={Styles.rightside}>
+        <Link href="/teacherhome" passHref>
+          <button className={Styles.home}> Dashboard </button>
+        </Link>
+        <Link href="/teacherinfopage" passHref>
+          <button className={Styles.moreinfo}> More info </button>
+        </Link>
+
+        <Link href="/" passHref>
+          <button className={Styles.logout}> Log out </button>
+        </Link>
+      </div>
     </div>
-    
-  )
+  );
 }
 export default Teachernavbar
 
