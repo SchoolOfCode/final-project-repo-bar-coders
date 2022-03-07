5afb0559d1dc90231a990c30886c32ee78798a67
Jenny Taylor <jennytaylor82@gmail.com>
2022-03-07 14:26:37 +0000

    teacher info page functionality complete

--- pages/_app.js
+++ pages/_app.js
@@ -78,10 +78,11 @@ function MyApp({ Component, pageProps }) {
  const [studentSelected, setStudentSelected] = useState({
    isSelected: false,
    id: null,
+   studentName: null,
  });
 
-  function changeStudentSelected(isSelected, id) {
-    setStudentSelected({isSelected: isSelected, id: id})
+  function changeStudentSelected(isSelected, id, name) {
+    setStudentSelected({isSelected: isSelected, id: id, studentName: name})
   }
 
   //Used in the book carousel (& other places?) Need to write fetch request to get data from database. Initial state is just an example to check code works
--- src/teacherComponents/IndividualSummaries/individualsummaries.js
+++ src/teacherComponents/IndividualSummaries/individualsummaries.js
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
+import styles from '../../../styles/studentstats.module.css'
 
 function IndividualSummaries({studentSelected}) {
   
@@ -24,21 +25,35 @@ function IndividualSummaries({studentSelected}) {
   
   
     return (
+      <div className={styles.studentSummaries}>
+        {!studentSelected.isSelected && (
+          <div className={styles.defaultText}>
+            <p>Select a student to see their individual summaries</p>
+          </div>
+        )}
+        <div className={styles.summaries}>
+          {summaries && (
+            <h2>{studentSelected.studentName}&apos;s reading summaries</h2>
+          )}
+          {summaries &&
+            summaries.map((entry, index) => {
+              return (
+                <div className={styles.summaryEntry} key={index}>
+                  <h3>
+                    {entry.title}, {entry.author}
+                  </h3>
+                  <h4>
+                    {formatDate(entry.date_created)}: {entry.number_pages}{" "}
+                    pages, {entry.minutes_read} minutes
+                  </h4>
 
-    <div>
-    {!studentSelected.isSelected && (<p>Select a student to see their individual summaries</p>)}
-    {summaries && summaries.map((entry) => {return (<div>
-    <h3>{formatDate(entry.date_created)}</h3>
-    <p>{entry.title}, {entry.author}</p>
-    <p>Pages Read: {entry.number_pages}, Minutes Read: {entry.minutes_read}</p>
-    <p>Summary: {entry.summary}</p>
-    </div>)}
-    
-    
-    )}
-    </div>
-
-  )
+                  <p>Summary: {entry.summary}</p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
 }
 
 export default IndividualSummaries
